806e4f9fd2b6ea1cc694bbaff27cd25220eaacbe
Shawn Presser <shawnpresser@gmail.com>
2022-06-05 16:18:10 -0500

    Add scatter test cases

--- src/npnd/_src/ops/scatter.py
+++ src/npnd/_src/ops/scatter.py
@@ -8,25 +8,25 @@ def _nd_indices(indices):
   indices = np.asarray(indices)
   return state_ops.batch_scatter_update_indices(np.expand_dims(indices, -1))[..., 1:].squeeze(-2)[..., ::-1]
 
-def _nd_indices(indices):
+def _nd_indices(indices, axis=0):
   indices = np.asarray(indices)
-  return state_ops.batch_scatter_indices(indices, axis=0)
+  return state_ops.batch_scatter_indices(indices, axis=axis)
 
-def _nd_flat_indices(indices):
+def _nd_flat_indices(indices, axis=0):
   indices = np.asarray(indices)
-  ind = _nd_indices(indices)
+  ind = _nd_indices(indices, axis=axis)
   strides = gather_nd.get_stride_sizes(ind.shape[:-1])
   ind = strides * gather_nd.flat_inner_dims(ind)
   ind = ind.sum(-1)
   ind = ind[..., None]
   return ind
 
-def scatter(params, indices, updates, reduction=None):
+def scatter(params, indices, updates, axis=0, reduction=None):
   params = np.asarray(params)
   indices = np.asarray(indices)
   updates = np.asarray(updates)
   # ind = _nd_indices(indices)
   #indices_flat = (gather_nd.get_stride_sizes(ind.shape[:-1]) * gather_nd.flat_inner_dims(ind)).sum(-1)[..., None]
-  indices_flat = _nd_flat_indices(indices)
-  result = scatter_nd.scatter_nd(params.flat, indices_flat, updates.flat)
+  indices_flat = _nd_flat_indices(indices, axis=axis)
+  result = scatter_nd.scatter_nd(params.flat, indices_flat, updates.flat, reduction=reduction)
   return result.reshape(params.shape)
--- tests/test_scatter_nd.py
+++ tests/test_scatter_nd.py
@@ -40,13 +40,64 @@ def check_scatter_nd(params, indices, updates, reduction=None, output=None, outp
   return y
 
 
+# The below Scatter's numpy implementation is from https://stackoverflow.com/a/46204790/11767360
+def scatter_ref(data, indices, updates, axis=0, reduction=None):  # type: ignore
+    assert reduction == None
+    if axis < 0:
+        axis = data.ndim + axis
+
+    idx_xsection_shape = indices.shape[:axis] + indices.shape[axis + 1:]
+
+    def make_slice(arr, axis, i):  # type: ignore
+        slc = [slice(None)] * arr.ndim
+        slc[axis] = i
+        return slc
+
+    def unpack(packed):  # type: ignore
+        unpacked = packed[0]
+        for i in range(1, len(packed)):
+            unpacked = unpacked, packed[i]
+        return unpacked
+
+    # We use indices and axis parameters to create idx
+    # idx is in a form that can be used as a NumPy advanced indices for scattering of updates param. in data
+    idx = [[unpack(np.indices(idx_xsection_shape).reshape(indices.ndim - 1, -1)),
+            indices[tuple(make_slice(indices, axis, i))].reshape(1, -1)[0]] for i in range(indices.shape[axis])]
+    idx = list(np.concatenate(idx, axis=1))
+    idx.insert(axis, idx.pop())
+
+    # updates_idx is a NumPy advanced indices for indexing of elements in the updates
+    updates_idx = list(idx)
+    updates_idx.pop(axis)
+    updates_idx.insert(axis, np.repeat(np.arange(indices.shape[axis]), np.prod(idx_xsection_shape)))
+
+    scattered = np.copy(data)
+    scattered[tuple(idx)] = updates[tuple(updates_idx)]
+    return scattered
+
+# verify scatter matches reference implementation.
+def check_scatter(params, indices, updates, axis=0, reduction=None, output=None, output_shape=None, desc=None):
+  params = np.asarray(params).astype(np.float)
+  indices = np.asarray(indices).astype(np.int64)
+  updates = np.asarray(updates).astype(np.float)
+  y = scatter(params, indices, updates, axis=axis, reduction=reduction)
+  if output_shape is not None:
+    x = np.asarray(output_shape)
+    y = np.asarray(y.shape)
+  elif output is not None:
+    x = np.asarray(output)
+  else:
+    x = scatter_ref(params, indices, updates, axis=axis, reduction=reduction)
+  ntu.check_eq(x, y)
+  return y
+
 def scatter_name_func(testcase_func, param_num, param):
   kwargs = dict(param.kwargs)
   name = testcase_func.__name__ + '_' + str(param_num)
   if 'desc' in kwargs:
     desc = kwargs.pop('desc')
     name += f"  {desc!r} "
-  args = ntu.get_call_args(check_scatter_nd, *param.args, **kwargs)
+  args = ntu.get_call_args(check_scatter, *param.args, **kwargs)
   indices = np.asarray(args['indices'])
   params = np.asarray(args['params'])
   updates = np.asarray(args['updates'])
@@ -71,9 +122,10 @@ def scatter_name_func(testcase_func, param_num, param):
   return name
 
 scatter_nd_test_case = functools.partial(ntu.parameterized.expand, name_func=scatter_name_func)
+scatter_test_case = functools.partial(ntu.parameterized.expand, name_func=scatter_name_func)
 
 
-class GatherNdTestCase(ntu.TestCase):
+class ScatterNdTestCase(ntu.TestCase):
 
   @scatter_nd_test_case([
     # ntu.param(
@@ -138,3 +190,45 @@ class GatherNdTestCase(ntu.TestCase):
   ])
   def test_scatter_nd(self, *args, **kws):
     check_scatter_nd(*args, **kws)
+
+class ScatterTestCase(ntu.TestCase):
+
+  @scatter_test_case([
+    *[
+      ntu.param(
+        desc = "ONNX ScatterElements example 1",
+        params = nnp.values((3,3,)),
+        indices = [
+          [1, 0, 2],
+          [0, 2, 1],
+        ],
+        updates = [
+          [1.0, 1.1, 1.2],
+          [2.0, 2.1, 2.2],
+        ],
+        # output = [
+        #   [2.0, 1.1, 0.0],
+        #   [1.0, 0.0, 2.2],
+        #   [0.0, 2.1, 1.2],
+        # ],
+        reduction = reduction
+      ) for reduction in [None] #, 'add', 'mul']
+    ],
+    *[
+      ntu.param(
+        desc = "ONNX ScatterElements example 2",
+        params = nnp.values((1,5,)),
+        indices = [[1, 3]],
+        updates = [[1.1, 2.1]],
+        axis = 1,
+        # output = [
+        #   [2.0, 1.1, 0.0],
+        #   [1.0, 0.0, 2.2],
+        #   [0.0, 2.1, 1.2],
+        # ],
+        reduction = reduction
+      ) for reduction in [None] #, 'add', 'mul']
+    ],
+  ])
+  def test_scatter(self, *args, **kws):
+    check_scatter(*args, **kws)
